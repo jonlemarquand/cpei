e5a42c9fcc36fe4b36d1474cf5b2e68a06c89701
Jon Le Marquand <jon.lemarquand@gmail.com>
2020-08-27 11:00:59 +0100

    Added results to table

--- src/containers/Results.js
+++ src/containers/Results.js
@@ -3,6 +3,8 @@ import  React,  { useState, useEffect } from 'react';
 import ResultsFilter from '../components/ResultsFilter/ResultsFilter';
 import ResultsTable from '../components/ResultsTable/ResultsTable';
 
+import cpeiData from '../data/cpeiData';
+
 import './Results.scss';
 
 const Results = () => {
@@ -17,6 +19,13 @@ const Results = () => {
         setIndustry(value)
     }
 
+    const AScores = cpeiData.map(company => {
+        if (company.results.[0].[theme] === "A" && (industry === 'all' || industry === company.industry)) {
+            return <div className="company">{company.display}</div>
+        }
+        return null;
+    })
+
     return (
         <div id="results">
             <h2>Results</h2>
@@ -27,7 +36,7 @@ const Results = () => {
                 industry={industry}
                 onIndustryChange={onIndustryChange}
             />
-            <ResultsTable />
+            <ResultsTable AScores={AScores} />
             </div>
         </div>
     )
